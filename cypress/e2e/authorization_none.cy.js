bdf5521c03fb438267b1c36f978405cbc605a93c
Waplll <106674268+Waplll@users.noreply.github.com>
2025-03-04 12:00:48 +0700

    сделал 4 вариант

--- cypress/e2e/authorization_none.cy.js
+++ cypress/e2e/authorization_none.cy.js
@@ -4,8 +4,8 @@ describe('authorization', () => {
             cy.log('переход на авторизацию')
             cy.visit('https://dev.profteam.su')
             cy.get('[href="/login"] > .button').click();
-            cy.get('.form-input--text').type(data.login)
-            cy.get('.form-input--password').type(data.password)
+            cy.get('.form-input--text').type(data.none_existent_login)
+            cy.get('.form-input--password').type(data.none_existent_pass)
             cy.wait(1000)
             cy.get(':nth-child(3) > .button').click()
 
